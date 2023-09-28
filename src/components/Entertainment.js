c93a1804eed1228a88606847edb1563b6e1ba670
Kornae Miller <me@Kornaes-MBP.lan>
2023-09-28 03:07:54 -0500

    Font Update

--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -1,3 +1,32 @@
+import FunctionsIcon from '@mui/icons-material/Functions';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import ScienceIcon from '@mui/icons-material/Science';
+
+
+import SchoolIcon from '@mui/icons-material/School';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import GavelIcon from '@mui/icons-material/Gavel';
+
+
+import PublicIcon from '@mui/icons-material/Public';
+import PetsIcon from '@mui/icons-material/Pets';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+
+
+
+import TvIcon from '@mui/icons-material/Tv';
+import TheatersIcon from '@mui/icons-material/Theaters';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import BoltIcon from '@mui/icons-material/Bolt';
+
+
+
+import ComputerIcon from '@mui/icons-material/Computer';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
+
 const entertainment = [
     {
         id: 1,
@@ -5,6 +34,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1589348972026-df051797cd66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTYxfHxIb2xseXdvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
         api: 'https://opentdb.com/api.php?amount=10&category=26&type=multiple&encode=url3986',
         category: 'Entertainment',
+        logo: <PeopleOutlineIcon fontSize='small' />
     },
     {
         id: 2,
@@ -12,6 +42,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1546608235-3310a2494cdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHNwb3J0c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
         api: 'https://opentdb.com/api.php?amount=10&category=21&type=multiple&encode=url3986',
         category: 'Entertainment',
+        logo: <SportsSoccerIcon fontSize='small' />
     },
     {
         id: 3,
@@ -20,6 +51,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1488485339565-566d63f7dbb7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=18&type=multiple&encode=url3986',
         category: 'Educational',
+        logo: <ComputerIcon fontSize='small' />
     },
     {
         id: 4,
@@ -27,6 +59,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1587181972975-ff4ecddeb30b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2864&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=28&type=multiple&encode=url3986',
         category: 'Misc',
+        logo: <TimeToLeaveIcon fontSize='small' />,
     },
     {
         id: 5,
@@ -34,6 +67,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=27&type=multiple&encode=url3986',
         category: 'Educational',
+        logo: <PetsIcon fontSize='small' />
     },
     {
         id: 6,
@@ -41,6 +75,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1554213352-5ffe6534af08?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2296&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=15&type=multiple&encode=url3986',
         category: 'Entertainment',
+        logo: <SportsEsportsIcon fontSize='small' />
     },
     {
         id: 7,
@@ -48,6 +83,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1515071970049-680dbd2a2339?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=24&type=multiple&encode=url3986',
         category: 'Misc',
+        logo: <GavelIcon fontSize='small' />
     },
     {
         id: 8,
@@ -56,6 +92,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1564769610726-59cead6a6f8f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2149&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=9&type=multiple&encode=url3986',
         category: 'Misc',
+        logo: <SchoolIcon fontSize='small' />
     },
     {
         id: 9,
@@ -63,6 +100,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1622542796254-5b9c46ab0d2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=25&type=multiple&encode=url3986',
         category: 'Misc',
+        logo: <ColorLensIcon fontSize='small' />
     },
     {
         id: 10,
@@ -70,6 +108,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1547555999-14e818e09e33?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2832&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=10&type=multiple&encode=url3986',
         category: 'Misc',
+        logo: <AutoStoriesIcon fontSize='small' />
     },
     {
         id: 11,
@@ -77,6 +116,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1519709042477-8de6eaf1fdc5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2992&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=22&type=multiple&encode=url3986',
         category: 'Educational',
+        logo: <LandscapeIcon fontSize='small' />
     },
     {
         id: 12,
@@ -85,6 +125,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1512551980832-13df02babc9e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=19&type=multiple&encode=url3986',
         category: 'Educational',
+        logo: <FunctionsIcon fontSize='small' />
     },
     {
         id: 13,
@@ -92,6 +133,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1526930700683-86db0d6bab09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTY2fHxzY2llbmNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
         api: 'https://opentdb.com/api.php?amount=10&category=17&type=multiple&encode=url3986',
         category: 'Educational',
+        logo: <ScienceIcon fontSize='small' />
     },
     {
         id: 14,
@@ -100,6 +142,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1553613600-cc662dc488f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2744&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=23&type=multiple&encode=url3986',
         category: 'Educational',
+        logo: <PublicIcon fontSize='small' />
     },
     {
         id: 15,
@@ -107,6 +150,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1551645120-d70bfe84c826?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2835&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=14&type=multiple&encode=url3986',
         category: 'Entertainment',
+        logo: <TvIcon fontSize='small' />
     },
     {
         id: 16,
@@ -115,6 +159,7 @@ const entertainment = [
         img: 'https://plus.unsplash.com/premium_photo-1676049462062-e8ec9b661675?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=11&type=multiple&encode=url3986',
         category: 'Entertainment',
+        logo: <TheatersIcon fontSize='small' />
     },
     {
         id: 17,
@@ -122,6 +167,7 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1567928513938-cd0e82a33ca3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=12&type=multiple&encode=url3986',
         category: 'Entertainment',
+        logo: <MusicNoteIcon fontSize='small' />
     },
     {
         id: 18,
@@ -129,7 +175,8 @@ const entertainment = [
         img: 'https://images.unsplash.com/photo-1554464901-78c9215b7e61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2874&q=80',
         api: 'https://opentdb.com/api.php?amount=10&category=20&type=multiple&encode=url3986',
         category: 'Misc',
-    }
+        logo: <BoltIcon fontSize='small' />
+    },
 ]
 
 export default entertainment;
